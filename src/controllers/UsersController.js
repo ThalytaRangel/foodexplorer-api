2727011efcd0a5b611744a6626dc23ff6991af26
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-21 20:58:26 -0300

    feat/TASK-0039: starting usersController and exception handling

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,17 @@
+const AppError = require('../utils/AppError')
+
+class UsersController {
+  async create(request, response) {
+    const {name, email, password} = request.body; 
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório") 
+    }
+    
+    await response.json({name, email, password});
+
+    
+  }
+}
+
+module.exports = UsersController;
