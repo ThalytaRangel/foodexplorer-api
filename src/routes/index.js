20e5b7472afba006e81a064e21ccc99b48d9f7bd
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-28 18:02:42 -0300

    feat/TASK:0044: Creation of sessionsController

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
-const dishesRouter = require("./dishes.routes")
+const dishesRouter = require("./dishes.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/dishes", dishesRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
