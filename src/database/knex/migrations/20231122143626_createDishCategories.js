861d186810278c13ab69dfc498f224cb70174585
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-28 20:32:31 -0300

    refactor/drop dish_categorys migrations

--- src/database/knex/migrations/20231122143626_createDishCategories.js
+++
@@ -1,7 +0,0 @@
-exports.up = (knex) =>
-  knex.schema.createTable("dish_categories", (table) => {
-    table.increments("id");
-    table.text("name").notNullable();
-  });
-
-exports.down = (knex) => knex.schema.dropTable("dish_categories");
