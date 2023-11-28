eb9d910837554f34217e373aef8a48e76824388f
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-28 00:07:32 -0300

    TASK:0043: Creation of all DishesControllers

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -41,6 +41,68 @@ class DishesController {
 
     return response.json({message: "Prato deletado"})
   }
+
+  async index(request, response) {
+    const {name} = request.query;
+
+    let dishes;
+
+    if(name) {
+      const dishesByName = await knex("dishes").whereLike('name', `%${name}%`).orderBy('name');
+      
+      if(dishesByName.length == 0) {
+        const dishesByIngredients = await knex("dish_ingredients").select("dishes.*").whereLike('dish_ingredients.name',`%${name}%`)
+        .innerJoin('dishes', 'dishes.id', 'dish_ingredients.dish_id').orderBy('dishes.name').groupBy('dishes.id');
+
+
+        dishes = dishesByIngredients;
+      } else {
+        dishes = dishesByName;
+      }
+    } else {
+      dishes = await knex("dishes").orderBy('name')
+    };
+
+    return response.json(dishes);
+  }
+
+  async update(request, response) {
+    const {id} = request.params;
+    const {name, description, price, category_id, ingredients} = request.body;
+
+    const dish = await knex("dishes").where({id}).first();
+
+    if (!dish) {
+      throw new AppError("Prato não encontrado!")
+    }
+
+    dish.name = name ?? dish.name;
+    dish.description = description ?? dish.description;
+    dish.category_id = category_id ?? dish.category_id;
+    dish.price = price ?? dish.price;
+
+    const ingredientsList = ingredients.map(ingredient => { 
+      return {
+        dish_id: id,
+        name: ingredient,
+      }
+    }) 
+
+        
+    await knex("dishes").where({id}).update({
+      name: dish.name, 
+      price: dish.price, 
+      description: dish.description, 
+      category_id: dish.category_id      
+    });
+
+    await knex("dish_ingredients").where({dish_id: id}).delete();
+
+    await knex("dish_ingredients").insert(ingredientsList);
+
+      
+    return response.json("Informações do prato atualizadas com sucesso!");
+  }
 }
 
 module.exports = DishesController;
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -8,6 +8,8 @@ const dishesController = new DishesController();
 
 dishesRoutes.post("/", dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
+dishesRoutes.get("/", dishesController.index);
+dishesRoutes.put("/:id", dishesController.update);
 dishesRoutes.delete("/:id", dishesController.delete);
 
 
