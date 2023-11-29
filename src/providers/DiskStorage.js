cdfe203d1938fad76ec0b3073a9202cf1d94c9d0
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-29 20:27:45 -0300

    feat/TASK-0047: Create DiskStorage.js

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const uploadsConfig = require("../configs/upload");
+
+class DiskStorage {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadsConfig.TMP_FOLDER, file), 
+      path.resolve(uploadsConfig.UPLOADS_FOLDER, file)
+    ); 
+    return file;
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadsConfig.UPLOADS_FOLDER, file);
+    try {
+      await fs.promises.stat(filePath);
+    }catch {
+      return
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
+
+module.exports = DiskStorage;
