4c8cc060b6d15194ea7d3e13dee7131652176cb8
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-11-30 22:50:58 -0300

    feat/FEAT-0001: create favorites routes

---
+++ src/routes/favorites.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+
+const FavoritesController = require("../controllers/FavoritesController");
+
+const ensureAutheticated = require("../middlewares/ensureAuthenticated");
+
+const favoritesRoutes = Router();
+
+const favoritesController = new FavoritesController();
+
+favoritesRoutes.use(ensureAutheticated);
+
+favoritesRoutes.get('/', favoritesController.index);
+favoritesRoutes.post('/:id', favoritesController.create);
+
+
+module.exports = favoritesRoutes;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const dishesRouter = require("./dishes.routes");
 const sessionsRouter = require("./sessions.routes");
+const favoritesRouter = require("./favorites.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/dishes", dishesRouter);
 routes.use("/sessions", sessionsRouter);
+routes.use("/favorites", favoritesRouter)
 
 module.exports = routes;
